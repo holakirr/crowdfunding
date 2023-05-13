5b84ae422413c37e8f857fee815af46172856806
holakirr <kpetunin@proton.me>
2023-05-13 17:52:19 -0600

    feat: Creates Sidebar component with navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home, Profile, CreateCampaign, CampaignDetails } from './pages';
+import { Navbar, Sidebar } from './components';
 
 const App = () => {
 	return (
 		<div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
-			<div className='sm:flex hidden mr-10 relative'>Sidebar</div>
+			<div className='sm:flex hidden mr-10 relative'>
+				<Sidebar />
+			</div>
 			<div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
-				Navbar
+				<Navbar />
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/about' element={<Profile />} />
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { logo, sun } from '../assets';
+import { navlinks } from '../constants';
+
+const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => {
+	console.log(isActive, name);
+	console.log(isActive === name);
+	const active = isActive === name;
+	return (
+		<div
+			className={`w-[48px] h-[48px] rounded-[10px] ${styles} ${
+				active && 'bg-[#2c2f32]'
+			} flex justify-center items-center ${!disabled && 'cursor-pointer'}`}
+			onClick={handleClick}
+		>
+			<img src={imgUrl} alt={name} className={`w-1/2 h-1/2 ${!active && 'grayscale'}`} />
+		</div>
+	);
+};
 
 function Sidebar() {
-	return <div>Sidebar</div>;
+	const navigate = useNavigate();
+	const [isActive, setIsActive] = useState('dashboard');
+
+	return (
+		<div className='flex justify-between items-center flex-col sticky top-5 h-[93vh]'>
+			<Link to='/'>
+				<Icon styles='w-[52px] h-[52px] bg-[#2c2f32]' imgUrl={logo} />
+			</Link>
+			<div className='flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12'>
+				<div className='flex flex-col justify-center items-center gap-3'>
+					{navlinks.map(link => (
+						<Icon
+							key={link.name}
+							{...link}
+							isActive={isActive}
+							handleClick={() => {
+								if (!link.disabled) {
+									setIsActive(link.name);
+									navigate(link.link);
+								}
+							}}
+						/>
+					))}
+				</div>
+				<Icon styles='bg-[#1c1c24] shadow-secondary' imgUrl={sun} />
+			</div>
+		</div>
+	);
 }
 
 export default Sidebar;
